242bac7fb19f00ffeeda4fc60e0e22c5aa0777ef
arafat2020 <arafatmannan9@gmail.cm>
2024-05-30 10:36:03 -0600

    initial schema defined

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AdminLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default AdminLayout
